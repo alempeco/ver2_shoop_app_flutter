0d709866d67964fd4f038235857334bdf6548b42
alempeco <79533382+alempeco@users.noreply.github.com>
2022-11-06 18:55:39 +0100

    Animation

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import './screens/edit_product_screen.dart';
 import 'providers/auth.dart';
 import 'screens/auth_screen.dart';
 import 'screens/splash_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,9 +45,13 @@ class MyApp extends StatelessWidget {
         builder: (ctx, auth, _) => MaterialApp(
           title: 'MyShop',
           theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.deepOrange,
             fontFamily: 'Lato',
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                .copyWith(secondary: Colors.deepOrange),
+            pageTransitionsTheme: PageTransitionsTheme(builders: {
+              TargetPlatform.android: CustomPageTransitionBuilder(),
+              TargetPlatform.iOS: CustomPageTransitionBuilder(),
+            }),
           ),
           home: auth.isAuth
               ? ProductsOverviewScreen()
